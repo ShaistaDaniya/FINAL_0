c6614f9173d046a54e81dc55ca37e1bfa9b656b5
ShaistaDaniya <shaista@gigandtake.com>
2023-07-16 01:12:06 +0530

    #6

--- LOGIN/Src/App.jsx
+++ LOGIN/Src/App.jsx
@@ -6,6 +6,7 @@ import Screen1 from './Screen1';
 import Screen2 from './Screen2';
 import Screen3 from './Screen3';
 import Screen4 from './Screen4';
+import Screen5 from './Screen5';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
           <Stack.Screen name="screen2" component={Screen2} />
           <Stack.Screen name="screen3" component={Screen3} />
           <Stack.Screen name="screen4" component={Screen4} />
+          <Stack.Screen name="screen5" component={Screen5} />
 
           
         </Stack.Navigator>
