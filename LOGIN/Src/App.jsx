d39e9d7345d1d706d7e3bc461539ab4d32365cba
ShaistaDaniya <shaista@gigandtake.com>
2023-07-16 00:22:26 +0530

    #3

--- LOGIN/Src/App.jsx
+++ LOGIN/Src/App.jsx
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Screen1 from './Screen1';
 import Screen2 from './Screen2';
-
+import Screen3 from './Screen3';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +15,8 @@ const App = () => {
         <Stack.Navigator initialRouteName="screen1" screenOptions={{ headerShown: false }}>
           <Stack.Screen name="screen1" component={Screen1} />
           <Stack.Screen name="screen2" component={Screen2} />
+          <Stack.Screen name="screen3" component={Screen3} />
+
           
         </Stack.Navigator>
       </NavigationContainer>
---
+++ LOGIN/Src/Screen4.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { Text, View, ActivityIndicator, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  verify: {
+    fontSize: 20,
+  },
+});
+
+const Screen4 = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.navigate('Screen5');
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.verify}>Verifying phone number</Text>
+      <ActivityIndicator size="large" color="#2F4D8B" />
+    </View>
+  );
+};
+
+export default Screen4;
