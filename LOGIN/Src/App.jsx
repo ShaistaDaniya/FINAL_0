5bf6b849b8313f51a5908232811959983d0f27a6
ShaistaDaniya <shaista@gigandtake.com>
2023-07-17 14:36:50 +0530

    #9

--- LOGIN/Src/App.jsx
+++ LOGIN/Src/App.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createStore } from 'redux';
-import rootReducer from './Reducer';
+import store from './store';
 import Screen1 from './Screen1';
 import Screen2 from './Screen2';
 import Screen3 from './Screen3';
 import Screen4 from './Screen4';
 import Screen5 from './Screen5';
-
-const store = createStore(rootReducer);
 
 const Stack = createNativeStackNavigator();
 
