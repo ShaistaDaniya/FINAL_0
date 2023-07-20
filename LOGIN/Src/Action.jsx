3f78a7ebbba7894409d052b88129b43e132f0dd4
ShaistaDaniya <shaista@gigandtake.com>
2023-07-21 01:59:22 +0530

    #13

--- LOGIN/Src/Action.jsx
+++ LOGIN/Src/Action.jsx
@@ -2,3 +2,8 @@ export const setPhoneNumber = (phoneNumber) => ({
   type: 'SET_PHONE_NUMBER',
   payload: phoneNumber,
 });
+
+export const getDataMethod = (data) => ({
+  type: 'GET_DATA',
+  payload: data
+})
--- LOGIN/Src/rootReducer.jsx
+++ LOGIN/Src/rootReducer.jsx
@@ -1,8 +1,13 @@
-// import { combineReducers } from 'redux';
-// import phoneNumberReducer from './Reducer'; 
-// const rootReducer = combineReducers({
-//   phoneNumber: phoneNumberReducer,
- 
-// });
+import { combineReducers } from 'redux';
+import phoneNumberReducer from './Reducer';
+import reducer from './Reducer' 
+import  ApiReducers  from './ApiReducer';
+const rootReducer = combineReducers({
+  phoneNumber: phoneNumberReducer,
+  reducer,
+  ApiReducers
 
-// export default rootReducer;
+ 
+});
+
+export default rootReducer;
